2ea613069b377ff0ef36fd7c69b09da0c1b3eb0c
dragos6 <99660191+dragos6@users.noreply.github.com>
2022-10-18 02:42:26 +0300

    just lively textures and jumpy logics

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -19,6 +20,10 @@ public class PlayerController : MonoBehaviour
 
     #region Movement
     private CharacterController controller;
+    private Vector3 pVelocity = Vector3.zero;
+
+    bool isBunnyHopping = false;
+    bool wishJump = false;
     #endregion
 
     #region SFX
@@ -49,13 +54,14 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
-
+        //Cursor
         if (Cursor.lockState != CursorLockMode.Locked)
         {
             if (Input.GetButtonDown("Fire1"))
                 Cursor.lockState = CursorLockMode.Locked;
         }
 
+        //Camera
         rotX -= Input.GetAxisRaw("Mouse Y") * xMouseSensitivity * 0.02f;
         rotY += Input.GetAxisRaw("Mouse X") * yMouseSensitivity * 0.02f;
 
@@ -65,17 +71,62 @@ public class PlayerController : MonoBehaviour
         }
         else if (rotX > clampXpos - clampLookDownOffset)
         {
-            rotX = clampXpos - clampLookDownOffset ;
+            rotX = clampXpos - clampLookDownOffset;
         }
         transform.rotation = Quaternion.Euler(0, rotY, 0);
         playerCamera.rotation = Quaternion.Euler(rotX, rotY, 0);
 
-        
+        //Movement
+        Jump();
+        if (controller.isGrounded) PlayerMove();
+        else if (!controller.isGrounded) PlayerMidAir();
+        controller.Move(pVelocity * Time.deltaTime);
 
-        // todo movement
-
-
-
+        //Camera follow after movement
         playerCamera.position = new Vector3(transform.position.x, transform.position.y + cameraYOffset, transform.position.z);
     }
+
+    private void Jump()
+    {
+        if(isBunnyHopping)
+        {
+            wishJump = Input.GetButton("Jump");
+            audioSource.PlayOneShot(jumpSfx);
+            return;
+        }
+
+        if(Input.GetButtonDown("Jump")&& !wishJump)
+        {
+            wishJump = true;
+        }
+        if(Input.GetButtonUp("Jump"))
+        {
+            wishJump = false;
+        }
+    }
+
+    private void PlayerMove()
+    {
+        Vector3 wishdir;
+
+        if(!wishJump)
+        {
+            ApplyFriction(1f);
+        }
+        else
+        {
+            ApplyFriction(0);
+        }
+    }
+
+    private void ApplyFriction(float v)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void PlayerMidAir()
+    {
+        throw new NotImplementedException();
+    }
+
 }
